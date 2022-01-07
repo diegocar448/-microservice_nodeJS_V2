a61de615e6744760024db35d419f4785f0f49a33
Diego Cardoso <diegocar448@hotmail.com>
2022-01-07 10:25:00 -0300

    adicionando estrutura no Route authorization.route.ts para geração do token

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -2,11 +2,24 @@ import { Router, Request, Response,  NextFunction} from 'express';
 import ForbiddenError from '../models/errors/forbidden.error.model';
 import userRepository from '../repositories/user.repository';
 import JWT from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
 
 
 const authorizationRoute = Router();
 
 authorizationRoute.post('/token', async (req:Request, res:Response, next:NextFunction) => {
+
+        // "iss" O domínio da aplicação geradora do token
+        // "sub" É o assunto do token, mas é muito utilizado para guarda o ID do usuário
+        // "aud" Define quem pode usar o token
+        // "exp" Data para expiração do token
+        // "nbf" Define uma data para qual o token não pode ser aceito antes dela
+        // "iat" Data de criação do token
+        // "jti" O id do token
+
+
+
+
     try {
         /* pegando o conteúdo do header authorization */
         const authorizationHeader = req.headers['authorization'];
@@ -34,16 +47,18 @@ authorizationRoute.post('/token', async (req:Request, res:Response, next:NextFun
         }
 
         const user  = await userRepository.findUsernameAndPassword(username, password);
-        
-        console.log(user);
 
-        // "iss" O domínio da aplicação geradora do token
-        // "sub" É o assunto do token, mas é muito utilizado para guarda o ID do usuário
-        // "aud" Define quem pode usar o token
-        // "exp" Data para expiração do token
-        // "nbf" Define uma data para qual o token não pode ser aceito antes dela
-        // "iat" Data de criação do token
-        // "jti" O id do token
+        if (!user) {
+            throw new ForbiddenError('Usuário ou senha inválidos');
+        }        
+
+        const jwtPayload = { username: user.username };
+        const jwtOptions = { subject: user?.uuid };
+        const secretKey = 'my_secret_key';
+
+        //vamos criar um token aqui
+        const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
+        res.status(StatusCodes.OK).json({ token:jwt });
 
     } catch (error) {
         next(error);
